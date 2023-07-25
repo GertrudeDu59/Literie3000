67e080a96407c4a0578dd0e4e655de054c0044a2
Ilyesse Touama <ilyesse.jv2@outlook.fr>
2023-07-25 11:36:46 +0200

    creation de la nav bar affichage des images et modification sql

--- index.php
+++ index.php
@@ -1,15 +1,56 @@
 <?php
-// Connexion à la base literie3000
-$dsn = "mysql:host=localhost;dbname=literie3000";
-$db = new PDO($dsn, "root", "");
+    // Connexion à la base literie3000
+    $dsn = "mysql:host=localhost;dbname=literie3000";
+    $db = new PDO($dsn, "root", "");
 
-// Récupérer les matelas de la table matelas
-$query = $db->query("SELECT * FROM matelas");
+    // Récupérer tous les matelas de la table matelas
+    $query = $db->query("SELECT * FROM matelas");
 
-// truc de tableau associatif
-$matelas = $query->fetchAll(PDO::FETCH_ASSOC);
-
+    // truc de tableau associatif
+    $matelas = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>literie3000</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://kit.fontawesome.com/63d06bf0f5.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="sticky-top py-1 bg-dark">
+    <div class="container d-flex justify-content-between">
+        <a class="py-2" href="#">
+          <img src="images/logo.png" class="logo" alt="">
+        </a>
+        <div class="box py-2">
+            <a class="py-2 d-none d-md-inline-block" href="#"><img src="images/cart.png" alt="" width="40%"></a>
+            <a class="py-2 d-none d-md-inline-block" href="#"><img src="images/user.png" alt="" width="50%"></i></a>
+        </div>
+    </div>
+</nav>
 
+
+
+  
+
+
+
+    <div class="recipes">
+    <?php
+    foreach ($matelas as $matela) {
+    ?>
+        <div class="recipe">
+            <img src="<?= $matela["images"] ?>" alt="">
+        </div>
+    <?php
+    }
+    ?>
+
+
+</body> 
+</html>
             
--- literie3000.sql
+++ literie3000.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE litererie3000;
+CREATE DATABASE literie3000;
 
-USE litererie3000;
+USE literie3000;
 
 CREATE TABLE matelas (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    marque VARCHAR(255) NOT NULL,
+    marque VARCHAR(60) NOT NULL,
     dimension VARCHAR(20) NOT NULL,
     prix DECIMAL(10, 2) NOT NULL,
-    image VARCHAR(255) NOT NULL
+    images VARCHAR(255) NOT NULL
 );
 
-INSERT INTO matelas (marque, dimension, prix, image) VALUES
-('EPEDA', '90x190', 759.00, 'epeda_90x190.jpg'),
-('DREAMWAY', '90x190', 809.00, 'dreamway_90x190.jpg'),
-('BULTEX', '140x190', 759.00, 'bultex_140x190.jpg'),
-('EPEDA', '160x200', 509.00, 'epeda_160x200.jpg');
+INSERT INTO matelas (marque, dimension, prix, images) VALUES
+('EPEDA', '90x190', 759.00, 'images/1.jpg'),
+('DREAMWAY', '90x190', 809.00, 'images/2.jpg'),
+('BULTEX', '140x190', 759.00, 'images/3.jpg'),
+('EPEDA', '160x200', 509.00, 'images/4.jpg');
